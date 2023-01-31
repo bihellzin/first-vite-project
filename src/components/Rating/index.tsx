e7fe33a9f490c92b5540ffad7208ebc9de2ba5e4
bielzin <oliva.gabriel@outlook.com>
2023-01-31 20:36:01 -0300

    adds previously developed project

---
+++ src/components/Rating/index.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import styles from './style.module.css';
+
+interface RatingProps {
+  setRate: React.Dispatch<React.SetStateAction<number | null>>
+}
+
+const Rating: React.FC<RatingProps> = ({ setRate }) => {
+  const [selectedRate, setSelectedRate] = useState<number | null>(null);
+  const rates = [1, 2, 3, 4, 5];
+
+  useEffect(() => {
+    selectedRate && setRate(selectedRate);
+  }, [selectedRate]);
+
+  const handleClickRate = (rate: number) => {
+    rate !== selectedRate && setSelectedRate(rate);
+    rate === selectedRate && setSelectedRate(null);
+  };
+
+  return (
+    <div className={styles.rating}>
+      {rates.map((rate) => (
+        <button
+          className={`${selectedRate === rate && styles.selectedrate}`}
+          onClick={() => handleClickRate(rate)}
+        >
+          {rate}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default Rating;
