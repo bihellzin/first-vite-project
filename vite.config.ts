15c7faa8f3c9796cbfe1a73c641960d04341bf4d
bielzin <oliva.gabriel@outlook.com>
2023-01-31 20:37:48 -0300

    fix path after repository renaming

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/first-vite-projet/",
+  base: "/first-vite-project/",
 });
